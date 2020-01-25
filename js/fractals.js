47a7c5e117db3205cb5ca84567429f593aed5927
My Lilja <my.s.lilja@gmail.com>
2020-01-25 16:13:41 +0100

    Trying some code

--- js/fractals.js
+++ js/fractals.js
@@ -12,15 +12,30 @@ let drawRect = function(x1, y1, size, size){
     ctx.stroke();
 }
 
-
-
+let startAngle = 45; 
 let size = 100;
-let angle = Math.PI/4;
+let angle = Math.PI/180;
 let strokeWidth = 10;
+let startX = width/2-size/2;
+let startY = height-strokeWidth;
+let nrBlocks = 5;
+ 
 
-drawRect(width/2-size/2, height-strokeWidth, size, size)
+drawRect(startX, startY, size, size)
 
-let calcSquares = function(x, y, size, angle, limit){
+let calcSquares = function(x, y, size, startAngle, angle, limit){
+
+    let lengthOfBlock = Math.sqrt(size*size/2); 
+    
+    let leftX2 = x - size *Math.cos(startAngle*angle)
+    let leftY2 = y - size *Math.sin(startAngle*angle)
+    //let rightX2 = x + size + lengthOfBlock*Math.cos(startAngle*angle)
+    //let rightY2 = y - lengthOfBlock*Math.sin(startAngle*angle)
+
+    drawRect(leftX2, leftY2, size, size)
+    drawRect(rightX2, rightY2, size, size)
 
 
 }
+
+calcSquares(startX, startY, size, startAngle, angle, nrBlocks)
