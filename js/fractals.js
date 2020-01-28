7a5fbcb81c2b0574a9ea9e6bd57890b09202b34b
Ryuu <evil__dark__angel@hotmail.com>
2020-01-28 18:06:24 +0100

    Update fractals.js

--- js/fractals.js
+++ js/fractals.js
@@ -70,7 +70,6 @@ let pyTree = {
 let color = {
     brown: "#654321",
     green: "#009a00",
-
 }
 
 draw();
@@ -79,7 +78,7 @@ draw();
 		ctx.clearRect(0, 0, width, height);
         pyTree.leftAngle = -Math.PI/4 + Math.sin(pyTree.time += 0.01) * Math.PI/4;
         pyTree.rightAngle = pyTree.leftAngle + 90*Math.PI / 180;
-		pyBlocks(width/2, height-50, pyTree.size, 0, 8, color.brown)
+		pyBlocks(width/2, height-50, pyTree.size, 0, 8, color.brown);
         requestAnimationFrame(draw);
         //return rightAngle;
     }
