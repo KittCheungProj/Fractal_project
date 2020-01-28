257cf76753539c37db5e9f27083338364626f7e4
My Lilja <my.s.lilja@gmail.com>
2020-01-28 10:18:20 +0100

    On our way

--- js/fractals.js
+++ js/fractals.js
@@ -4,8 +4,12 @@ width = canvas.width =window.innerWidth
 height = canvas.height = window.innerHeight
 
 //let startAngle = 0 + Math.random()*Math.PI/4*90;
-let leftAngle = -45 * Math.PI/180;
+let leftAngle = Math.PI/4;
+let totalDegrees = Math.PI/2;
+let rightAngle = Math.abs(totalDegrees - leftAngle)
 let size = 100;
+console.log('ett   ' + leftAngle)
+console.log('ett1  '+totalDegrees)
 
 let pyTree = function(x, y, size, angle){
     
@@ -14,34 +18,43 @@ let pyTree = function(x, y, size, angle){
     ctx.rotate(angle)
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, size, -size);
+    console.log('size   '+size)
     
-    let leftSize = size * Math.abs(1-(leftAngle/90));
+    let leftDiff = Math.abs(1-(leftAngle/totalDegrees));
+    console.log('två   '+leftDiff)
+    let leftSize = Math.abs(Math.sqrt((size*size)*leftDiff));
+    //let leftSize = size * Math.abs(1-(leftAngle/90));
     let x1 = 0;
     let y1 = -size;
-    //let leftAngle = startAngle;
-    //let leftDiff = Math.abs(1-(leftAngle/-90));
+    console.log('tre    '+ leftSize);
+    
+    
+    let rightDiff = Math.abs(1-leftDiff);
+    let rightSize = Math.abs(Math.sqrt((size*size)*rightDiff));
+    let x2 = x1+Math.cos(rightAngle)*rightSize;
+    let y2 = y1+Math.sin(rightAngle)*rightSize;
     
         
     if (leftSize > 10){
-      pyTree(x1, y1, leftSize/1.5, leftAngle);
+      pyTree(x1, y1, leftSize, -leftAngle);
 
     }else{
         ctx.save();
       	ctx.translate(x1, y1);
       	ctx.rotate(leftAngle);
-      	ctx.fillRect(0, 0, leftSize/ 1.5, -leftsize /1.5);
+      	ctx.fillRect(0, 0, leftSize, -leftSize);
       	ctx.restore();
     }
     
-    //pyTree(rightX2, rightY2, rightSize, rightAngle)
-   // } else{
-   //     ctx.save();
-   // 	ctx.translate(x1, y1);
-   // 	ctx.rotate(leftAngle);
-   // 	ctx.fillRect(0, 0, size0, -size0);
-   // 	ctx.restore();
-//
-   // }
+    if(rightSize > 10){
+        pyTree(x2, y2, rightSize, rightAngle)
+    }else{
+        ctx.save();
+    	ctx.translate(x1, y1);
+    	ctx.rotate(leftAngle);
+    	ctx.fillRect(0, 0, size, -size);
+    	ctx.restore();
+   }
 
         
     // }
