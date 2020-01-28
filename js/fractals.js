bff0c8db73d4f84e6b309fd38782e3f0225646ce
Ryuu <evil__dark__angel@hotmail.com>
2020-01-28 12:03:18 +0100

    testing tree

--- js/fractals.js
+++ js/fractals.js
@@ -4,13 +4,14 @@ width = canvas.width =window.innerWidth
 height = canvas.height = window.innerHeight
 
 
-let leftAngle = Math.PI/4;
-let totalDegrees = Math.PI/2;
-let rightAngle = totalDegrees - leftAngle;
+let leftAngle = -70* Math.PI/180;
+let totalDegrees = 90*Math.PI/180;
+let rightAngle = leftAngle + Math.PI / 2;
+//Math.abs(totalDegrees + leftAngle);
 let size = 100;
 
 
-let pyTree = function(x, y, size, angle){
+let pyTree = function(x, y, size, angle, limit){
     
     ctx.save();
     ctx.translate(x, y);
@@ -19,34 +20,35 @@ let pyTree = function(x, y, size, angle){
     ctx.fillRect(0, 0, size, -size);
 
     
-    let leftDiff = Math.abs(1-(leftAngle/totalDegrees));
-
-    let leftSize = Math.abs(Math.sqrt((size*size)*leftDiff));
+    let leftDiff = Math.abs(1-(leftAngle/-totalDegrees));
+    console.log(leftDiff)
+    let leftSize = (Math.sqrt((size*size)*leftDiff));
 
     let x1 = 0;
     let y1 = -size;
 
     
     
-    let rightDiff = Math.abs(1-leftDiff);
+    let rightDiff = (1-leftDiff);
+    console.log('r' + rightDiff)
     let rightSize = Math.abs(Math.sqrt((size*size)*rightDiff));
-    let x2 = x1 + Math.cos(rightAngle) * rightSize;
-    let y2 = y1 + Math.sin(-rightAngle) * rightSize;
+    let x2 = x1 + Math.cos(leftAngle) * leftSize;
+    let y2 = y1 + Math.sin(leftAngle) * leftSize;
     
         
-    if (leftSize > 10){
-      pyTree(x1, y1, leftSize, -leftAngle);
+    if (limit > 0){
+      pyTree(x1, y1, leftSize, leftAngle, limit-1);
 
     }else{
         ctx.save();
       	ctx.translate(x1, y1);
-      	ctx.rotate(-leftAngle);
+      	ctx.rotate(leftAngle);
       	ctx.fillRect(0, 0, leftSize, -leftSize);
       	ctx.restore();
     }
 
-    if(rightSize > 10){
-        pyTree(x2, y2, rightSize, rightAngle)
+    if(limit > 0){
+        pyTree(x2, y2, rightSize, rightAngle+ 0.18, limit-1)
     }else{
       ctx.save();
     	ctx.translate(x2, y2);
@@ -56,6 +58,6 @@ let pyTree = function(x, y, size, angle){
    }
     ctx.restore();
 }
-pyTree(width/2, height, size, 0)
+pyTree(width/2, height, size, 0, 8)
 
 
